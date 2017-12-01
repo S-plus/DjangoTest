8b5ab06c0548e4e0e823a512a1fabc664f429a37
S-plus <splusljh@foxmail.com>
2017-12-01 17:21:39 +0800

    example3_1

--- HelloDjango/views.py
+++ HelloDjango/views.py
@@ -3,7 +3,31 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("at the HelloDjango index")
+    # return HttpResponse("at the HelloDjango index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    template = loader.get_template('HelloDjango/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse("you're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "you're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("you're voting on question %s." % question_id)
