276d7192deac00d42480b94996a136de010da5a4
S-plus <splusljh@foxmail.com>
2017-12-05 16:21:37 +0800

    example4

--- HelloDjango/views.py
+++ HelloDjango/views.py
@@ -1,33 +1,72 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
-
-
-def index(request):
-    # return HttpResponse("at the HelloDjango index")
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-    template = loader.get_template('HelloDjango/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+from .models import Question, Choice
 
 
-def detail(request, question_id):
-    return HttpResponse("you're looking at question %s." % question_id)
+# def index(request):
+#     # return HttpResponse("at the HelloDjango index")
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # output = ', '.join([q.question_text for q in latest_question_list])
+#     # return HttpResponse(output)
+#     template = loader.get_template('HelloDjango/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+#
+#
+# def detail(request, question_id):
+#     # return HttpResponse("you're looking at question %s." % question_id)
+#
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'HelloDjango/detail.html', {'question': question})
+#
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'HelloDjango/results.html', {'question': question})
 
 
-def results(request, question_id):
-    response = "you're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class IndexView(generic.ListView):
+    template_name = 'HelloDjango/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'HelloDjango/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'HelloDjango/results.html'
 
 
 def vote(request, question_id):
-    return HttpResponse("you're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'HelloDjango/detail.html', {
+            'question': question,
+            'error_message': "you didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('HelloDjango:results', args=(question.id,)))
+
